f4f3c8d771259bd8366e96f577a346a4dc170904
Nicholas Kekoa Nii <35237477+NicholasNii2018@users.noreply.github.com>
2018-01-29 12:51:14 -1000

    Completed SW2

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -3,3 +3,30 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 3
+var goodGrade = "Ive been doing all my work"
+if goodGrade == "Ive been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+//Part 4
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+//Part 5:
+ var gradeA = true
+
+if gradeA {
+    print("I really like this class")
+}
+else
+{
+    print("I’m not so sure about coding")
+}
+
